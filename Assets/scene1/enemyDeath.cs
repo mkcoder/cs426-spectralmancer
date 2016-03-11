7b97da052601ac283876e90b3defa2b0d0a18573
Bresia Prudente <macanrox@gmail.com>
2016-03-10 20:13:13 -0600

    Uploading script for enemy death

---
+++ Assets/scene1/enemyDeath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//change Player to the name of the controllable character
+	public void onCollisionEnter (Collision node) {
+		if(node.gameObject.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
