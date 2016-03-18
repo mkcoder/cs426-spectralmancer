a144b3e36e990cbc26d08df810ea99284d304b9a
Garret Felker <garretfelker@gmail.com>
2016-03-18 12:03:28 -0500

    pushing the reboot

---
+++ asgn8/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public int movespeed = 50;
+    public int rotatespeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * rotatespeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotatespeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * movespeed * Time.deltaTime);
+        }
+    }
+}
---
+++ asgn8/Assets/Scripts/Necromancer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Necromancer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.color = Color.green;
+    }
+	
+	// Update is called once per frame
+	void Update() { }
+        
+}
