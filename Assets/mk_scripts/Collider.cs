262676df391eeb2e2874e75110e14725b557a746
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2016-03-30 12:16:27 -0500

    fixed the colliders to work with multiple string list, and also add a rigidbody to object that don't have it.

--- Assets/mk_scripts/Collider.cs
+++ Assets/mk_scripts/Collider.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Collider : MonoBehaviour
 {
-    public string col_name;
+    public List<string> NamesList;
 
 
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start ()
+    {
+        if (GetComponent<Rigidbody>() == null)
+        {
+            var r = gameObject.AddComponent<Rigidbody>();
+            r.useGravity = false;
+            r.isKinematic = false;
+            r.mass = 2.37f;
+            r.drag = 6.67f;
+            r.angularDrag = 2.66f;
+            r.interpolation = RigidbodyInterpolation.None;
+            ;
+            r.collisionDetectionMode = CollisionDetectionMode.Discrete;
+
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,27 +33,32 @@ public class Collider : MonoBehaviour
 
     public void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name.Equals(col_name))
+        if (canDestroy(col.gameObject.name))
         {
-            Debug.Log(col_name + " has [enter] collidied");
             Destroy(gameObject);
+            Debug.Log(gameObject.name + " has [start] collidied");
         }
     }
-
     
-
     public void OnCollisionStay(Collision col)
     {
-        if (col.gameObject.name.Equals(col_name))
+        if (canDestroy(col.gameObject.name))
         {
-            Debug.Log(col_name + " has [stay] collidied");
+            // remove life points if possible 
+            Debug.Log(gameObject.name + " has [stay] collidied");
         }
     }
     public void OnCollisionExit(Collision col)
     {
-        if (col.gameObject.name.Equals(col_name))
+        if (canDestroy(col.gameObject.name))
         {
-            Debug.Log(col_name + " has [exit] collidied");
+            // add a lighting sounds play here
+            Debug.Log(gameObject.name + " has [exit] collidied");
         }
+    }
+
+    public bool canDestroy(string name)
+    {
+        return NamesList.Exists(_ => _.Equals(name));  
     }
 }
