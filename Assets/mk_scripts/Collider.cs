f6683579415f803d649188e94b0767b65a548c4b
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2016-03-30 10:08:08 -0500

    colliders works

---
+++ Assets/mk_scripts/Collider.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider : MonoBehaviour
+{
+    public string col_name;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Equals(col_name))
+        {
+            Debug.Log(col_name + " has [enter] collidied");
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+    public void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.name.Equals(col_name))
+        {
+            Debug.Log(col_name + " has [stay] collidied");
+        }
+    }
+    public void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.name.Equals(col_name))
+        {
+            Debug.Log(col_name + " has [exit] collidied");
+        }
+    }
+}
