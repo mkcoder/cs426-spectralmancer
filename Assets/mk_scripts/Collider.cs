866b8bc007b94816cde04a5b0f45ecb157080f84
Bresia Prudente <macanrox@gmail.com>
2016-03-31 17:06:51 -0500

    Updated billboards -> removed billboard texts -> added new terrain, dead grass -> added dead tree -> applied billboard to dust particles

---
+++ Assets/mk_scripts/Collider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collider : MonoBehaviour
+{
+    public List<string> NamesList;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        NamesList = new List<string>();
+        NamesList.Add("LightningBall");
+        if (GetComponent<Rigidbody>() == null)
+        {
+            var r = gameObject.AddComponent<Rigidbody>();
+            r.useGravity = false;
+            r.isKinematic = false;
+            r.mass = 2.37f;
+            r.drag = 6.67f;
+            r.angularDrag = 2.66f;
+            r.interpolation = RigidbodyInterpolation.None;
+            ;
+            r.collisionDetectionMode = CollisionDetectionMode.Discrete;
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (canDestroy(col.gameObject.name))
+        {
+            Destroy(gameObject);
+            Debug.Log(gameObject.name + " has [start] collidied");
+        }
+    }
+    
+    public void OnCollisionStay(Collision col)
+    {
+        if (canDestroy(col.gameObject.name))
+        {
+            // remove life points if possible 
+            Debug.Log(gameObject.name + " has [stay] collidied");
+        }
+    }
+    public void OnCollisionExit(Collision col)
+    {
+        if (canDestroy(col.gameObject.name))
+        {
+            // add a lighting sounds play here
+            Debug.Log(gameObject.name + " has [exit] collidied");
+        }
+    }
+
+    public bool canDestroy(string name)
+    {
+        return NamesList.Exists(_ => _.Equals(name));  
+    }
+}
---
+++ Assets/mk_scripts/MK_AI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MK_AI : MonoBehaviour
+{
+
+    private GameObject teen;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("rightCornerWalkingPath"), "time", 1000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
