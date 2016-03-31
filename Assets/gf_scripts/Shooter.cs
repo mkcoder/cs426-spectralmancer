663418a4a470431bf38ff6d52762d271e41b04e2
Bresia Prudente <macanrox@gmail.com>
2016-03-31 05:12:37 -0500

    Added dust particle material, prefab, and texture under bp_files.

---
+++ Assets/gf_scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+    public Rigidbody    projectile;
+    public Transform    projectilePos;
+
+    void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            Rigidbody missile = Instantiate(projectile, projectilePos.position, projectilePos.rotation) as Rigidbody;
+            missile.AddForce(transform.forward * 200);
+        }
+    }
+}
