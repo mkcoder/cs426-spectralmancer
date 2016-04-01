8a54b8046847e67d524fa5e714c1c3c0e47212d3
Garret Felker <garretfelker@gmail.com>
2016-04-01 13:16:45 -0500

    Last Push for Assignment9

--- Assets/gf_scripts/Shooter.cs
+++ Assets/gf_scripts/Shooter.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
     public Rigidbody    projectile;
     public Transform    projectilePos;
+    public Rigidbody    skeleton;
 
     void Update () {
 
@@ -11,6 +12,11 @@ public class Shooter : MonoBehaviour {
         {
             Rigidbody missile = Instantiate(projectile, projectilePos.position, projectilePos.rotation) as Rigidbody;
             missile.AddForce(transform.forward * 200);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Rigidbody summoned = Instantiate(skeleton, projectilePos.position, projectilePos.rotation) as Rigidbody;
         }
     }
 }
