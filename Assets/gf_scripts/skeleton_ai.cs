800f794008a1c3ad4437353d7777d7326f14dda8
Garret Felker <garretfelker@gmail.com>
2016-04-01 12:29:26 -0500

    Added skeletons, AI still under construction

---
+++ Assets/gf_scripts/skeleton_ai.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class skeleton_ai : MonoBehaviour {
+
+    private Renderer rend;
+    List<Transform> enemyList;
+    float min;
+    float dist;
+    Transform target;
+
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.material.color = Color.grey;
+
+        Object.Destroy(gameObject, 10.0f);
+    }
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        enemyList.Clear();
+
+        foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("teen"))
+        {
+            enemyList.Add(enemy.transform);
+        }
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("dog"))
+        {
+            enemyList.Add(enemy.transform);
+        }
+        min = 9999999;
+        foreach(Transform location in enemyList)
+        {
+            dist = Vector3.Distance(gameObject.transform.position, location.position);
+            if (dist < min)
+            {
+                min = dist;
+                target = location;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
+
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
+
+    }
+
+
+}
