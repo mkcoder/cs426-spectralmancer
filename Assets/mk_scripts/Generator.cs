a14dad422d25cbb700cfb8cb8a55d0febb236a00
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2016-03-31 22:33:51 -0500

    added generator and ai script

---
+++ Assets/mk_scripts/Generator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// this generates Teens and Dogs at random (x,y,z) position and add a Path.
+/// </summary>
+public class Generator : MonoBehaviour
+{
+    GameObject _teen, _iTeen, _dog, _iDog;
+
+
+    public int frameToGenerateTeenDog = 10000;
+    private int currentFrameCount = 0;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    // create a teen
+	    for (int i = 0; i < 1; i++)
+        {
+            _dog = GameObject.Find("dog");
+            _teen = GameObject.Find("teen");
+            _iTeen = (GameObject)Instantiate(_teen, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            _iTeen.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+	}
+
+    // Update is called once per frame
+	void Update ()
+	{
+        if ( currentFrameCount++ % frameToGenerateTeenDog == 0 )
+	        GenerateGameObject();
+	}
+
+    private void GenerateGameObject()
+    {
+        // create a teen
+        for (int i = 0; i < 1; i++)
+        {
+            _iTeen = (GameObject)Instantiate(_teen, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            _iTeen.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+    }
+}
--- Assets/mk_scripts/MK_AI.cs
+++ Assets/mk_scripts/MK_AI.cs
@@ -1,17 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class MK_AI : MonoBehaviour
 {
 
     private GameObject teen;
+    public int time;
+    private string _pathChoice = "rightCornerWalkingPath";
+
+    public void setPathChoice(string pathChoice)
+    {
+        _pathChoice = pathChoice;
+    }
 
 	// Use this for initialization
 	void Start ()
 	{
-	    iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("rightCornerWalkingPath"), "time", 1000));
-	}
-	
+	    iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(_pathChoice), "time", time));
+	}    
+
 	// Update is called once per frame
 	void Update () {
 	
