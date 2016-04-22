e2dcdc348beb3d0e8ceb51e738144c63dc06992a
Garret Felker <garretfelker@gmail.com>
2016-04-21 23:24:10 -0500

    Added skeleton attack animations

--- Assets/gf_scripts/skeleton_ai.cs
+++ Assets/gf_scripts/skeleton_ai.cs
@@ -2,48 +2,23 @@
 using System.Collections.Generic;
 
 public class skeleton_ai : MonoBehaviour {
-    List<Transform> enemyList;
-    float min;
-    float dist;
-    Transform target;
-
 
     // Use this for initialization
     void Start () {
-
         Object.Destroy(gameObject, 10.0f);
     }
 	
+    void Update ()
+    {
 
+    }
 
-	// Update is called once per frame
-	//void Update () {
-        /*enemyList.Clear();
-
-        foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("teen"))
+    public void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "dog" || col.gameObject.tag == "kitten".Substring(0, 6))
         {
-            enemyList.Add(enemy.transform);
+            this.GetComponent<Animation>().Play("attack");
         }
-        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("dog"))
-        {
-            enemyList.Add(enemy.transform);
-        }
-        min = 9999999;
-        foreach(Transform location in enemyList)
-        {
-            dist = Vector3.Distance(gameObject.transform.position, location.position);
-            if (dist < min)
-            {
-                min = dist;
-                target = location;
-                transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
-            }
-        }
-        transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
-
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
-        */
-    //}
-
+    }
 
 }
