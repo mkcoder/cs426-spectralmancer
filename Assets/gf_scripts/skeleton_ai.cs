7ba37734a3a173bfcf0691ac5b12c27f346c45bd
Garret Felker <garretfelker@gmail.com>
2016-04-22 03:15:58 -0500

    Fixed Chain Lightning -Spell complete

--- Assets/gf_scripts/skeleton_ai.cs
+++ Assets/gf_scripts/skeleton_ai.cs
@@ -8,21 +8,31 @@ public class skeleton_ai : MonoBehaviour {
     // Use this for initialization
     void Start () {
         audio = GetComponent<AudioSource>();
-        Object.Destroy(gameObject, 10.0f);
+
+        Object.Destroy(gameObject, 15.0f);
+        
+        StartCoroutine(deathCoroutine());
     }
 	
     void Update ()
     {
-
     }
 
     public void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "dog" || col.gameObject.tag == "kitten".Substring(0, 6))
+        if (col.gameObject.tag == "dog" || col.gameObject.tag == "kitten".Substring(0, 6)
+            || col.gameObject.tag == "teen")
         {
             this.GetComponent<Animation>().Play("attack");
             audio.PlayOneShot(swords, 1);
         }
+        else if (col.gameObject.tag == "lightning")
+            Object.Destroy(gameObject);
     }
 
+    System.Collections.IEnumerator deathCoroutine()
+    {
+        yield return new WaitForSeconds(13);
+        this.GetComponent<Animation>().Play("die");
+    }
 }
